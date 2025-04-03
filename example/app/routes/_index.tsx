ce53fbb1ae95fc0023ef2de52f642d170ba6174b
Andriy Massimilla <andriygm@icloud.com>
2025-04-03 17:36:59 -0400

    Add tailwind + images to example app

--- example/app/routes/_index.tsx
+++ example/app/routes/_index.tsx
@@ -1,46 +1,26 @@
-import type { MetaFunction } from "react-router";
-import { Link } from "react-router";
-
-export const meta: MetaFunction = () => {
-	return [
-		{ title: "New Remix App" },
-		{ name: "description", content: "Welcome to Remix!" },
-	];
-};
+import Link, { ExternalLink } from "~/components/link";
 
 export default function Index() {
-	return (
-		<div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-			{/* TODO: find new react router tutorial */}
-			<h1>Welcome to React Router</h1>
-			<ul>
-				<li>
-					<a
-						target="_blank"
-						href="https://remix.run/tutorials/blog"
-						rel="noreferrer"
-					>
-						15m Quickstart Blog Tutorial
-					</a>
-				</li>
-				<li>
-					<a
-						target="_blank"
-						href="https://remix.run/tutorials/jokes"
-						rel="noreferrer"
-					>
-						Deep Dive Jokes App Tutorial
-					</a>
-				</li>
-				<li>
-					<a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-						Remix Docs
-					</a>
-				</li>
-				<li>
-					<Link to="/posts">Posts with loader</Link>
-				</li>
-			</ul>
-		</div>
-	);
+  return (
+    <>
+      <Link to="/posts">Posts with loader</Link>
+
+      <ul>
+        <li>
+          {/* TODO: find new react router tutorial */}
+          <ExternalLink href="https://remix.run/tutorials/blog">
+            15m Quickstart Blog Tutorial
+          </ExternalLink>
+        </li>
+        <li>
+          <ExternalLink href="https://remix.run/tutorials/jokes">
+            Deep Dive Jokes App Tutorial
+          </ExternalLink>
+        </li>
+        <li>
+          <ExternalLink href="https://remix.run/docs">Remix Docs</ExternalLink>
+        </li>
+      </ul>
+    </>
+  );
 }
